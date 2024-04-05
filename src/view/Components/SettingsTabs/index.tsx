7c86bd8b253c3b5689496620e72bbfd6afea23f4
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-04-05 15:32:06 -0300

    feat: responsive tabs

--- src/view/Components/SettingsTabs/index.tsx
+++ src/view/Components/SettingsTabs/index.tsx
@@ -1,48 +1,73 @@
 import * as Tabs from '@radix-ui/react-tabs';
 import { TabItem } from './TabItem';
 import { useState } from 'react';
-
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 export function SettingsTabs() {
   const [currentTab, setCurrentTab] = useState('tab-1');
   return (
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
-      <Tabs.List className="mt-6 flex items-center gap-4 border-b border-zinc-200">
-        <TabItem
-          value="tab-1"
-          isSelected={currentTab === 'tab-1'}
-          title="My details"
-        />
-        <TabItem
-          value="tab-2"
-          isSelected={currentTab === 'tab-2'}
-          title="Profile"
-        />
-        <TabItem
-          value="tab-3"
-          isSelected={currentTab === 'tab-3'}
-          title="Password"
-        />
-        <TabItem
-          value="tab-4"
-          isSelected={currentTab === 'tab-4'}
-          title="Team"
-        />
-        <TabItem
-          value="tab-5"
-          isSelected={currentTab === 'tab-5'}
-          title="Plan"
-        />
-        <TabItem
-          value="tab-6"
-          isSelected={currentTab === 'tab-6'}
-          title="Billing"
-        />
-        <TabItem
-          value="tab-7"
-          isSelected={currentTab === 'tab-7'}
-          title="Email"
-        />
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="hover">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex items-center gap-4 border-b border-zinc-200">
+            <TabItem
+              value="tab-1"
+              isSelected={currentTab === 'tab-1'}
+              title="My details"
+            />
+            <TabItem
+              value="tab-2"
+              isSelected={currentTab === 'tab-2'}
+              title="Profile"
+            />
+            <TabItem
+              value="tab-3"
+              isSelected={currentTab === 'tab-3'}
+              title="Password"
+            />
+            <TabItem
+              value="tab-4"
+              isSelected={currentTab === 'tab-4'}
+              title="Team"
+            />
+            <TabItem
+              value="tab-5"
+              isSelected={currentTab === 'tab-5'}
+              title="Plan"
+            />
+            <TabItem
+              value="tab-6"
+              isSelected={currentTab === 'tab-6'}
+              title="Billing"
+            />
+            <TabItem
+              value="tab-7"
+              isSelected={currentTab === 'tab-7'}
+              title="Email"
+            />
+            <TabItem
+              value="tab-8"
+              isSelected={currentTab === 'tab-8'}
+              title="Email"
+            />
+            <TabItem
+              value="tab-9"
+              isSelected={currentTab === 'tab-9'}
+              title="Email"
+            />
+            <TabItem
+              value="tab-10"
+              isSelected={currentTab === 'tab-10'}
+              title="Email"
+            />
+          </Tabs.List>
+        </ScrollArea.Viewport>
+        <ScrollArea.Scrollbar
+          className="flex h-1 translate-y-1.5 touch-none select-none flex-col bg-zinc-100"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   );
 }
--- src/view/Layouts/LayoutApp.tsx
+++ src/view/Layouts/LayoutApp.tsx
@@ -3,9 +3,9 @@ import { Sidebar } from '../Components/Sidebar';
 
 export default function LayoutApp() {
   return (
-    <div className="grid min-h-screen grid-cols-app">
+    <div className="min-h-screen lg:grid lg:grid-cols-app">
       <Sidebar />
-      <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-4 lg:pb-12 lg:pt-8">
+      <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-4 lg:pb-12 lg:pt-8">
         <Outlet />
       </main>
     </div>
