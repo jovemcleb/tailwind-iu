0e361a3b890e2a06e46b4a14a51a3c17594daf9a
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-04-05 15:12:19 -0300

    feat(Sidebar): implements open and close sidebar

--- src/view/Components/Sidebar/index.tsx
+++ src/view/Components/Sidebar/index.tsx
@@ -1,36 +1,49 @@
-import { Cog, LifeBuoy, Search } from 'lucide-react';
+import { Cog, LifeBuoy, Menu, Search } from 'lucide-react';
 import { Logo } from '../Logo';
 import { MainNavigation } from './MainNavigation';
 import { NavItem } from './MainNavigation/NavItem';
 import UsedSpaceWidget from './UsedSpaceWidget';
 import { Profile } from './Profile';
 import * as Input from '../Input';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { Button } from '../Button';
 
 export function Sidebar() {
   return (
-    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4   lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
-
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
-
-      <MainNavigation />
-
-      <div className="mt-auto flex flex-col gap-6">
-        <nav className="space-y-0.5">
-          <NavItem icon={LifeBuoy} title="Suport" />
-          <NavItem icon={Cog} title="Settings" />
-        </nav>
-        <UsedSpaceWidget />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:h-screen">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
       </div>
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </Input.Prefix>
+          <Input.Control placeholder="Search" />
+        </Input.Root>
 
-      <div className="h-px bg-zinc-200" />
+        <MainNavigation />
 
-      <Profile />
-    </aside>
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem icon={LifeBuoy} title="Suport" />
+            <NavItem icon={Cog} title="Settings" />
+          </nav>
+          <UsedSpaceWidget />
+        </div>
+
+        <div className="h-px bg-zinc-200" />
+
+        <Profile />
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
--- src/view/Layouts/LayoutApp.tsx
+++ src/view/Layouts/LayoutApp.tsx
@@ -5,7 +5,7 @@ export default function LayoutApp() {
   return (
     <div className="grid min-h-screen grid-cols-app">
       <Sidebar />
-      <main className="px-4 pb-12 pt-8">
+      <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-4 lg:pb-12 lg:pt-8">
         <Outlet />
       </main>
     </div>
