5ff71ed99f58f2387e3df456b76381e425f2b226
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-04-10 08:29:56 -0300

    fix: aply color in tabs trigger

--- src/view/Components/SettingsTabs/TabItem.tsx
+++ src/view/Components/SettingsTabs/TabItem.tsx
@@ -12,7 +12,7 @@ export function TabItem({ title, value, isSelected = false }: TabItemProps) {
       value={value}
       className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700 dark:text-zinc-300 dark:hover:text-violet-300 dark:data-[state=active]:text-violet-300"
     >
-      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4 dark:text-zinc-400">
+      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
         {title}
       </span>
       {isSelected && (
